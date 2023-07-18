b22fc075e1e5cfc40572b816bf7ef8310ade4937
Rpyaanng <RyanPang12@gmail.com>
2023-07-18 15:18:06 -0600

    added install script

---
+++ Install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Setting things up. This might take a minute
+cd ~
+sudo apt update
+sudo apt install zsh curl wget git
+
+echo Run ./zsh.sh after running zsh shell to complete installation.
---
+++ zsh.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/zsh
+# installs oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# installs oh my zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# install brew
+ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/rpyaanng/.zprofile
+
+sudo apt-get update
+sudo apt-get install build-essential
+
+# install latest neovim
+brew install neovim
+
+# install my .files
+cd ~
+git clone https://github.com/rpyaanng/.files
+cd .files
+cp -r bin .tmux.conf .zshrc ~/
+
+# get my neovim config
+mkdir ~/.config
+cp -r nvim ~/.config
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cd ~
+
+# brew install fzf find-fd and ripgrep for neovim and tmux sessionizer
+sudo apt install fd-find
+brew install fzf
+brew install ripgrep
+
+read -p "Do you want to proceed? (yes/no) " yn
+
+case $yn in 
+	yes ) sudo apt install wslu;;
+	no ) echo exiting...;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
+echo Finished. Restart terminal to see zsh changes.
